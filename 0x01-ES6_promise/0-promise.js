2ad7ef99d35f2b3de905d9288a1b610a0805a501
Allanwils <zionalehebrew@gmail>
2023-06-27 14:57:22 +0300

    task 0 promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,5 @@
-// 0-promise.js
-export function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform API request or any asynchronous operation here
-    
-    // If the operation is successful, resolve the promise
-    resolve(/* Your response data */);
-    
-    // If there's an error, reject the promise
-    // reject(new Error("Error message"));
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
   });
 }
