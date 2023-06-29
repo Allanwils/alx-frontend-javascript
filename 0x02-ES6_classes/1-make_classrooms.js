9e9bc8581cbaec545c0b490cbcd74bd9208b1502
Allanwils <zionalehebrew@gmail>
2023-06-29 09:43:51 +0300

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const classrooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
+
+  return classrooms;
+}
+
+export default initializeRooms;
