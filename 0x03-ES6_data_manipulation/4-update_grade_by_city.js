2487be9493f626e7a8b396982eedf91187df0d75
Allanwils <zionalehebrew@gmail>
2023-07-04 09:16:04 +0300

    attempting task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,18 @@
-import getListStudents from "./0-get_list_students.js";
-
 function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const matchingGrade = newGrades.find(
-        (grade) => grade.studentId === student.id
-      );
-      const grade = matchingGrade ? matchingGrade.grade : "N/A";
-      return { ...student, grade };
-    });
+  const studentsInCity = students.filter((student) => student.location === city);
+  return studentsInCity.map((student) => {
+    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+    if (filteredGrades.length > 0) {
+      return {
+        ...student,
+        grade: filteredGrades[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
 }
 
 export default updateStudentGradeByCity;
