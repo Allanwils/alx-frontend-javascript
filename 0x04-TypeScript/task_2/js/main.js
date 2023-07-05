532e0ad453acc70c8b49681e1377da8091650cae
Allanwils <zionalehebrew@gmail>
2023-07-05 19:46:52 +0300

    task 6

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -2,13 +2,13 @@ var Director = /** @class */ (function () {
     function Director() {
     }
     Director.prototype.workFromHome = function () {
-        return "Working from home";
+        return 'Working from home';
     };
     Director.prototype.getCoffeeBreak = function () {
-        return "Getting a coffee break";
+        return 'Getting a coffee break';
     };
     Director.prototype.workDirectorTasks = function () {
-        return "Getting to director tasks";
+        return 'Getting to director tasks';
     };
     return Director;
 }());
@@ -16,27 +16,37 @@ var Teacher = /** @class */ (function () {
     function Teacher() {
     }
     Teacher.prototype.workFromHome = function () {
-        return "Cannot work from home";
+        return 'Cannot work from home';
     };
     Teacher.prototype.getCoffeeBreak = function () {
-        return "Cannot have a break";
+        return 'Cannot have a break';
     };
     Teacher.prototype.workTeacherTasks = function () {
-        return "Getting to work";
+        return 'Getting to work';
     };
     return Teacher;
 }());
 function createEmployee(salary) {
-    if (typeof salary === "number" && salary < 500) {
+    if (typeof salary === 'number' && salary < 500) {
         return new Teacher();
     }
     else {
         return new Director();
     }
 }
-console.log(createEmployee(200));
-// Output: Teacher
-console.log(createEmployee(1000));
-// Output: Director
-console.log(createEmployee("$500"));
-// Output: Director
+function isDirector(employee) {
+    return employee.workDirectorTasks !== undefined;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    }
+    else {
+        console.log(employee.workTeacherTasks());
+    }
+}
+console.log(createEmployee(200)); // Teacher
+console.log(createEmployee(1000)); // Director
+console.log(createEmployee('$500')); // Director
+executeWork(createEmployee(200)); // Getting to work
+executeWork(createEmployee(1000)); // Getting to director tasks
